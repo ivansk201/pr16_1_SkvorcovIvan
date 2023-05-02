0133ec4ca82f9faf15256b36983e76526d2a09f1
BulatDes <bulatdespizek@gmail.com>
2023-05-02 15:47:29 +0500

    Практика 16 (1-2 задания)

---
+++ pkract16console/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace pkract16console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите элементы массива с использованием \"/\": ");
+            string[] arr = Console.ReadLine().Split('/');
+            int digCount = 0;
+            foreach (string s in arr) {
+                foreach (char c in s) {
+                    if (char.IsDigit(c)) { digCount++; }
+                }
+
+            }
+            Console.WriteLine($"Кол-во цифр в массиве: {digCount}");
+            Console.WriteLine($"Элементы массива до символа \"/\": ");
+            foreach (string s in arr) {
+                if (s == "/")
+                {
+                    break;
+                }
+                if (s != "") { Console.WriteLine(s); }
+               
+            }
+            Console.WriteLine($"Элементы массива после символа \"/\" в верхнем и нижнем регистрах:");
+            foreach (string s in arr) {
+                if (s == "/") { continue; }
+                if (s == "") { break; }
+                string news = "";
+                foreach (char c in s)
+                {
+                    if (Char.IsUpper(c))
+                    {
+                        news += Char.ToLower(c);
+                    }
+                    else if (Char.IsLower(c))
+                    {
+                        news += Char.ToUpper(c);
+                    }
+                    else { news += c; }
+                }
+                Console.WriteLine(news);
+                Console.WriteLine("Измененный массив записан в файл");
+                StreamWriter st = new StreamWriter("mass.txt", true);
+                st.WriteLine(news);
+                st.Close();
+            }
+           
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pkract16forms/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace pkract16forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string slovo = textBox1.Text.ToLower();
+            int count = 0;
+            using (StreamReader rd = new StreamReader("massiv.txt"))
+            {            
+                string line;
+                while ((line = rd.ReadLine()) != null) 
+                {
+                    string[] words = line.Split(' ');
+                    foreach (string word in words) 
+                    {
+                        if (word.ToLower() == slovo)
+                        {
+                            count++;
+                        }                       
+                    }
+                    if (count == 0)
+                    {
+                        MessageBox.Show("Слово не найдено", "Ошибка");
+                    }
+                        label5.Text = line;
+                }
+                rd.Close();
+                label1.Text = $"Было найдены {count} вхождения (ий) поискового запроса \"{textBox1.Text}\"";
+                label1.Visible = true;
+                
+            }
+        }
+    }
+}
